7c270b229f1b61a96e8990911e6a3d7b9a4e96b5
unknown <ariast390@gmail.com>
2022-06-23 15:19:58 +0700

    fixing navbar tispage

--- src/pages/tipsKarir.tsx
+++ src/pages/tipsKarir.tsx
@@ -1,7 +1,29 @@
-const TipsKarir = () => {
-    return(
-        <div>Ini Halaman Tips Karir</div>
-    )
-}
+import { Box } from "@chakra-ui/react";
+import Navbar from "../components/navbar";
+// import React from "react";
+// @ts-ignore
+import Intro from "../components/tipsSections/section/HeroLanding.tsx";
+// @ts-ignore
+import Arrow from "../components/tipsSections/section/ScrollMenu";
+// @ts-ignore
+import { Services } from "../components/tipsSections/section/services";
+// import { Services } from "./sections/services";
+// import { Commitments } from "./sections/commitments";
+// import { HowWeWork } from "./sections/how-we-work";
+// import { Testimonials } from "./sections/testimonials";
 
-export default TipsKarir;
+export default function Landing() {
+  return (
+    <Box>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Arrow />
+
+      {/* <Services /> */}
+      {/* <Commitments /> */}
+      {/* <HowWeWork /> */}
+      {/* <Testimonials /> */}
+    </Box>
+  );
+}
