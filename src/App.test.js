0051092e4cf026d4383d668599caa97b06d925bd
Linear-DX <mariolinear97@gmail.com>
2022-07-10 18:24:38 +0700

    Fix test error

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,15 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from './pages/home.tsx';
 import Search from './components/heading.tsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 test('Testing text SMKJOB', () => {
   render(
     <ChakraProvider>
+      <Provider store={store}>
       <Home />
+      </Provider>
     </ChakraProvider>
   );
   const linkElement = screen.getAllByText('SMKJOB');
